580afa546cb8ba9c9281a3bf3cc371957955f1de
_ <bbb31@users.noreply.github.com>
2017-12-03 17:37:02 -0500

    build cleanup

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 env GOOS=darwin GOARCH=amd64 go build -o slurp-macos-amd64
 env GOOS=darwin GOARCH=arm64 go build -o slurp-macos-arm64
 env GOOS=linux GOARCH=amd64 go build -o slurp-linux-amd64
-env GOOS=windows GOARCH=386 go build -o slurp-windows-386
+env GOOS=windows GOARCH=386 go build -o slurp-windows-386.exe
