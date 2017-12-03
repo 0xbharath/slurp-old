049e00722c3b31278bff5b85773f930326a4d5ec
_ <bbb31@users.noreply.github.com>
2017-12-03 17:33:02 -0500

    add build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+env GOOS=darwin GOARCH=amd64 go build -o slurp-macos-amd64
+env GOOS=darwin GOARCH=arm64 go build -o slurp-macos-arm64
+env GOOS=linux GOARCH=amd64 go build -o slurp-linux-amd64
+env GOOS=windows GOARCH=386 go build -o slurp-windows-386
